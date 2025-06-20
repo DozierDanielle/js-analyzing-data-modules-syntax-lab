b92e7a8d56bec81f55138743035eab2f8153464d
Dozier Danielle <dozierdanielle@ymail.com>
2025-06-20 17:58:26 -0500

    Complete combineUser lab

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+// 1. Bring in the datejs library
+const datejs = require('datejs');
 
+// 2. Create your function
+function combineUsers(...args) {
+  const combinedObject = { users: [] };
 
+  for (const arr of args) {
+    combinedObject.users = [...combinedObject.users, ...arr];
+  }
 
+  combinedObject.merge_date = Date.today().toString("M/d/yyyy");
+
+  return combinedObject;
+}
+
+// 3. Export the function for testing
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
